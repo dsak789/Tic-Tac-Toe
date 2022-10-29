91e5d2f1cb27b97506261750cc6e5f4cd245c856
Dannana Sai Ajith Kumar <99498718+sbcreations14378@users.noreply.github.com>
2022-10-30 01:47:34 +0530

    Add files via upload
    Disabled Images After Position Filled

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cc1=document.getElementById("c1");
+cc1=document.getElementById("c1");
 var cc2=document.getElementById("c2");
 var cc3=document.getElementById("c3");
 var cc4=document.getElementById("c4");
@@ -9,95 +9,134 @@ var cc8=document.getElementById("c8");
 var cc9=document.getElementById("c9");
 var rst=document.getElementById("rsrt");
 var count=0
+t1=document.getElementById("td1");
+
+
 
 function c1(){
-    cc1.src="tttX.jpg";
+   
     if(count%2==0){
-        
+        cc1.src="tttX.jpg";
+        cc1.onclick="";
+        cc1.style.cursor="not-allowed";
         count=count+1;
     }
     else{
         cc1.src="tttO.jpg";
+        cc1.onclick="";
+        cc1.style.cursor="not-allowed";
         count=count+1;
     }
 }
 function c2(){
     if(count%2==0){
         cc2.src="tttX.jpg";
+        cc2.onclick="";
+        cc2.style.cursor="not-allowed";
         count=count+1;
     }
     else{
         cc2.src="tttO.jpg";
+        cc2.onclick="";
+        cc2.style.cursor="not-allowed";
         count=count+1;
     }
 }
 function c3(){
     if(count%2==0){
         cc3.src="tttX.jpg";
+        cc3.onclick="";
+        cc3.style.cursor="not-allowed";
         count=count+1;
     }
     else{
         cc3.src="tttO.jpg";
+        cc3.onclick="";
+        cc3.style.cursor="not-allowed";
         count=count+1;
     }
 }
 function c4(){
     if(count%2==0){
         cc4.src="tttX.jpg";
+        cc4.onclick="";
+        cc4.style.cursor="not-allowed";
         count=count+1;
     }
     else{
         cc4.src="tttO.jpg";
+        cc4.onclick="";
+        cc4.style.cursor="not-allowed";
         count=count+1;
     }
 }
 function c5(){
     if(count%2==0){
         cc5.src="tttX.jpg";
+        cc5.onclick="";
+        cc5.style.cursor="not-allowed";
         count=count+1;
     }
     else{
         cc5.src="tttO.jpg";
+        cc5.onclick="";
+        cc5.style.cursor="not-allowed";
         count=count+1;
     }
 }
 function c6(){
     if(count%2==0){
         cc6.src="tttX.jpg";
+        cc6.onclick="";
+        cc6.style.cursor="not-allowed";
         count=count+1;
     }
     else{
         cc6.src="tttO.jpg";
+        cc6.onclick="";
+        cc6.style.cursor="not-allowed";
         count=count+1;
     }
 }
 function c7(){
     if(count%2==0){
         cc7.src="tttX.jpg";
+        cc7.onclick="";
+        cc7.style.cursor="not-allowed";
         count=count+1;
     }
     else{
         cc7.src="tttO.jpg";
+        cc7.onclick="";
+        cc7.style.cursor="not-allowed";
         count=count+1;
     }
 }
 function c8(){
     if(count%2==0){
         cc8.src="tttX.jpg";
+        cc8.onclick="";
+        cc8.style.cursor="not-allowed";
         count=count+1;
     }
     else{
         cc8.src="tttO.jpg";
+        cc8.onclick="";
+        cc8.style.cursor="not-allowed";
         count=count+1;
     }
 }
 function c9(){
     if(count%2==0){
         cc9.src="tttX.jpg";
+        cc9.onclick="";
+        cc9.style.cursor="not-allowed";
         count=count+1;
     }
     else{
         cc9.src="tttO.jpg";
+        cc9.onclick="";
+        cc9.style.cursor="not-allowed";
         count=count+1;
     }
 }
@@ -109,5 +148,15 @@ function start(){
     }
     else{
         rst.innerHTML="Start";
+    }
+}
+
+
+function checkwin(){
+    if(cc1.src==cc2.src==cc3.src && cc1.src!="smile.jpg"){
+        alert("win");
+    }
+    else{
+        alert("ongoing")
     }
 }
