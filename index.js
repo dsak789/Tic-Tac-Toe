466fa6fbe75d6eb9b3060ebc9c4a4a43c96911ff
dsak789 <dsak.official@gmail.com>
2024-08-17 00:47:13 +0530

    TIC TAC TOE Game Complete 2 Players

--- index.js
+++ index.js
@@ -1,162 +1,65 @@
-cc1=document.getElementById("c1");
-var cc2=document.getElementById("c2");
-var cc3=document.getElementById("c3");
-var cc4=document.getElementById("c4");
-var cc5=document.getElementById("c5");
-var cc6=document.getElementById("c6");
-var cc7=document.getElementById("c7");
-var cc8=document.getElementById("c8");
-var cc9=document.getElementById("c9");
 var rst=document.getElementById("rsrt");
 var count=0
-t1=document.getElementById("td1");
+var cells = []
 
-
-
-function c1(){
-   
-    if(count%2==0){
-        cc1.src="tttX.jpg";
-        cc1.onclick="";
-        cc1.style.cursor="not-allowed";
-        count=count+1;
-    }
-    else{
-        cc1.src="tttO.jpg";
-        cc1.onclick="";
-        cc1.style.cursor="not-allowed";
-        count=count+1;
-    }
-}
-function c2(){
-    if(count%2==0){
-        cc2.src="tttX.jpg";
-        cc2.onclick="";
-        cc2.style.cursor="not-allowed";
-        count=count+1;
-    }
-    else{
-        cc2.src="tttO.jpg";
-        cc2.onclick="";
-        cc2.style.cursor="not-allowed";
-        count=count+1;
-    }
-}
-function c3(){
-    if(count%2==0){
-        cc3.src="tttX.jpg";
-        cc3.onclick="";
-        cc3.style.cursor="not-allowed";
-        count=count+1;
-    }
-    else{
-        cc3.src="tttO.jpg";
-        cc3.onclick="";
-        cc3.style.cursor="not-allowed";
-        count=count+1;
-    }
-}
-function c4(){
-    if(count%2==0){
-        cc4.src="tttX.jpg";
-        cc4.onclick="";
-        cc4.style.cursor="not-allowed";
-        count=count+1;
-    }
-    else{
-        cc4.src="tttO.jpg";
-        cc4.onclick="";
-        cc4.style.cursor="not-allowed";
-        count=count+1;
-    }
-}
-function c5(){
-    if(count%2==0){
-        cc5.src="tttX.jpg";
-        cc5.onclick="";
-        cc5.style.cursor="not-allowed";
-        count=count+1;
-    }
-    else{
-        cc5.src="tttO.jpg";
-        cc5.onclick="";
-        cc5.style.cursor="not-allowed";
-        count=count+1;
-    }
-}
-function c6(){
-    if(count%2==0){
-        cc6.src="tttX.jpg";
-        cc6.onclick="";
-        cc6.style.cursor="not-allowed";
-        count=count+1;
-    }
-    else{
-        cc6.src="tttO.jpg";
-        cc6.onclick="";
-        cc6.style.cursor="not-allowed";
-        count=count+1;
-    }
-}
-function c7(){
-    if(count%2==0){
-        cc7.src="tttX.jpg";
-        cc7.onclick="";
-        cc7.style.cursor="not-allowed";
-        count=count+1;
-    }
-    else{
-        cc7.src="tttO.jpg";
-        cc7.onclick="";
-        cc7.style.cursor="not-allowed";
-        count=count+1;
-    }
-}
-function c8(){
-    if(count%2==0){
-        cc8.src="tttX.jpg";
-        cc8.onclick="";
-        cc8.style.cursor="not-allowed";
-        count=count+1;
-    }
-    else{
-        cc8.src="tttO.jpg";
-        cc8.onclick="";
-        cc8.style.cursor="not-allowed";
-        count=count+1;
-    }
-}
-function c9(){
-    if(count%2==0){
-        cc9.src="tttX.jpg";
-        cc9.onclick="";
-        cc9.style.cursor="not-allowed";
-        count=count+1;
-    }
-    else{
-        cc9.src="tttO.jpg";
-        cc9.onclick="";
-        cc9.style.cursor="not-allowed";
-        count=count+1;
-    }
-}
-function start(){
-    cc1.src=cc2.src=cc3.src=cc4.src=cc5.src=cc6.src=cc7.src=cc8.src=cc9.src="smile.jpg";
-    rst.innerHTML;
-    if(rst.innerHTML=="Start"){
-        rst.innerHTML="Restart";
-    }
-    else{
-        rst.innerHTML="Start";
-    }
+for(var i = 1; i<=9; i++){
+    cells.push(document.getElementById('c'+i))
 }
 
 
-function checkwin(){
-    if(cc1.src==cc2.src==cc3.src && cc1.src!="smile.jpg"){
-        alert("win");
+function C(btn_Id){
+    if(count%2==0){
+        btn_Id.src="tttX.jpg";
     }
     else{
-        alert("ongoing")
+        btn_Id.src="tttO.jpg";
     }
+    btn_Id.onclick=null;
+    btn_Id.style.cursor="not-allowed";
+    count++;
+    checkwin()
+    
 }
+function start() {
+    cells.forEach(function(cell) {
+        cell.src = "smile.jpg";
+        cell.onclick = function() { C(cell); };
+        cell.style.cursor = "pointer";
+    });
+
+    rst.innerHTML = (rst.innerHTML == "Start") ? "Restart" : "Start";
+    count = 0;
+}
+function checkwin(){    
+    var winPatterns = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6]              // Diagonals
+    ];
+
+    for(var i=0; i < winPatterns.length; i++){
+        var [a,b,c] = winPatterns[i]
+        if(cells[a].src === cells[b].src && cells[a].src === cells[c].src && cells[a].src.includes('ttt')){
+           if(cells[a].src.includes('tttX')){
+            alert("Player X won the Match")
+        }
+        else if(cells[a].src.includes('tttO')){
+            alert("Player O won the Match")
+        }
+        if(confirm("Restart the Game")){
+            start()
+            window.location.href = "./"
+        }
+        }
+        
+    rst.innerHTML = "Restart";
+    }
+    if(count === 9){
+        alert("Draw Match")
+    }
+
+}
+
+
+
+
